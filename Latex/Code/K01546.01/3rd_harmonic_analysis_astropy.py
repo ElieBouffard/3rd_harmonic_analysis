9ced0bea5424ac95cd4651c7109a7a511de7a3a4
ElieBouffard <elie.bouffard@usherbrooke.ca>
2016-08-16 14:51:23 -0400

    Update 3rd_harmonic_analysis_astropy.py

--- Latex/Code/K01546.01/3rd_harmonic_analysis_astropy.py
+++ Latex/Code/K01546.01/3rd_harmonic_analysis_astropy.py
@@ -12,8 +12,7 @@ from astropy.stats import LombScargle
 #load the fits files, then write the planet period, the time of its first
 #transit and its duration. If the number of files is not 17, then you will
 #have to change reading loop iteration number as well as the concatenate part.
-#By default, the periodograms uses frequencies from 10.**(-0.05)*orbital_frequency
-#to 10**(0.8)*orbital_frequency with 2000 points evenly spaced in log space.
+#This version uses AtroPy's L-S module
 
 hdulist = []
 
